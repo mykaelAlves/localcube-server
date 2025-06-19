d4b9446b0d539f994194fc645fe852a6ddab5b12
makai <mykaelalvesnogueira1@gmail.com>
2025-06-18 22:14:04 -0300

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::convert::Infallible;
+use std::fs;
 
 use serde::Deserialize;
 use http_body_util::Full;
@@ -9,17 +10,23 @@ use hyper::{
     Request, Response
 };
 
+#[derive(Debug, Deserialize)]
 struct Config {
     address: Address
 }
 
+#[derive(Debug, Deserialize)]
 struct Address {
     ip: Vec<u8>,
     port: u16,
 }
 
 fn main() {
-    println!("Hello, world!");
+    let config: Config = toml::from_str(
+        &fs::read_to_string("config/Config.toml").unwrap()
+    ).unwrap();
+
+    println!("{:#?}", config);
 }
 
 async fn hello(_: Request<body::Incoming>) 
